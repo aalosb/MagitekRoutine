408b376702401f7f5e4d07553875524f8bd5bc6a
aalosb <32321566+aalosb@users.noreply.github.com>
2024-06-29 00:50:50 -0500

    remove cg-pre check for now.

--- MagitekLoader/MagitekLoader.cs
+++ MagitekLoader/MagitekLoader.cs
@@ -229,7 +229,7 @@ public class CombatRoutineLoader : CombatRoutine
         _latestVersion = GetLatestVersion().Result;
         var latest = _latestVersion;
 
-        if (local == latest || latest == null || local.StartsWith("cg-pre-"))
+        if (local == latest || latest == null /*|| local.StartsWith("cg-pre-")*/)
         {
             LoadProduct();
             return;
