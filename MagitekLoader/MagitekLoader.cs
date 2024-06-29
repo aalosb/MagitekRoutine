57430e589e8b3e3fa06041d5158d184fc70f90d4
aalosb <32321566+aalosb@users.noreply.github.com>
2024-06-29 00:26:17 -0500

    update loader to point to github actions

--- MagitekLoader/MagitekLoader.cs
+++ MagitekLoader/MagitekLoader.cs
@@ -21,8 +21,8 @@ public class CombatRoutineLoader : CombatRoutine
     private const string ProjectName = "Magitek";
     private const string ProjectMainType = "Magitek.Magitek";
     private const string ProjectAssemblyName = "Magitek.dll";
-    private const string ZipUrl = "https://ddjx48xxp2d6i.cloudfront.net/Magitek.zip";
-    private const string VersionUrl = "https://ddjx48xxp2d6i.cloudfront.net/Version.txt";
+    private const string ZipUrl = "https://github.com/cheesegoldfish/MagitekRoutine/releases/latest/download/Magitek.zip";
+    private const string VersionUrl = "https://github.com/cheesegoldfish/MagitekRoutine/releases/latest/download/Version.txt";
     private static readonly Color LogColor = Colors.CornflowerBlue;
 
     private static readonly string ProjectAssembly = Path.Combine(Environment.CurrentDirectory, $@"Routines\{ProjectName}\{ProjectAssemblyName}");
